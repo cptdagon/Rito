9cae2c5a2e18afeddb0010edfee94822446e72c1
Adam Rushby <adam.b.rushby@gmail.com>
2018-04-25 13:35:41 +0100

    Update RitoMain.py

--- RitoMain.py
+++ RitoMain.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 
 
 def main():
-    api = Ritoapi('RGAPI-18fb1348-b7de-4d46-bd9f-cd30d4fa6786')
+    api = Ritoapi('')
     #r = api.get_summoner_by_name('Captain DaGOn')
     #a = api.get_rank_by_summonerid(r['id'])
     #b = api.get_past_20_ranked_solo(r['accountId'])
